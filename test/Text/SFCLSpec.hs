816183a35e0139dc8fb7826c948abcf7e702ed9e
Benjamin Kaestner <benjamin.kaestner@gmail.com>
2015-09-15 07:00:01 +0200

    Add random song tests

--- test/Text/SFCLSpec.hs
+++ test/Text/SFCLSpec.hs
@@ -1,5 +1,6 @@
 {-# LANGUAGE OverloadedStrings #-}
 module Text.SFCLSpec (spec) where
+import           Control.Applicative
 import           Data.Attoparsec.ByteString.Char8
 import           Data.ByteString.Char8 (ByteString)
 import qualified Data.ByteString.Char8 as B
@@ -24,6 +25,10 @@ spec = describe "songParser" $ do
   it "parses both versions to the correct AST" $ do
       songFormatted ~> songParser `shouldParse` song
       songLines     ~> songParser `shouldParse` song
+
+  it "provides an identity" $ property $ \s ->
+    let k = B.pack $ toFormattedSong s
+    in k ~> songParser `shouldParse` s
 
 
 song :: Song
@@ -70,3 +75,33 @@ songFormatted = B.unlines
   , "@chord{Eb} copy them into"
   , "@chord{Bb} Word?"
   ]
+
+--------------------------------------------------------------------
+
+instance Arbitrary Song where
+  arbitrary = resize 120 $ Song <$> listOf1 arbitrary
+  -- shrink (Song xs) = map (Song . pure) xs
+
+instance Arbitrary Line where
+  arbitrary = resize 4 $ Line <$> listOf1 arbitrary
+  -- shrink (Line xs) = map (Line . pure) xs
+
+instance Arbitrary Block where
+  arbitrary = do
+    k <- choose (1, 3) :: Gen Int
+    let syms    = elements $ ['a'..'z'] ++ ['A'..'Z']
+    let trimmed = unwords <$> listOf1 (listOf1 syms)
+    let chord   = trimmed
+    let lyrics  = trimmed
+    case k of
+      1 -> CLBlock <$> chord <*> lyrics
+      2 -> CBlock <$> chord
+      3 -> LBlock <$> lyrics
+
+toFormattedSong :: Song -> String
+toFormattedSong (Song xs) = unlines . map ln $ xs
+  where
+    ln (Line ls)     = unlines . map bl $ ls
+    bl (CLBlock c l) = "@chord{" ++ c ++ "} " ++ l
+    bl (CBlock c) = "@chord{" ++ c ++ "}"
+    bl (LBlock l) = l
