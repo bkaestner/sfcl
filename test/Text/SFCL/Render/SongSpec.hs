9352ebcdf567d6a65b22a52702cbe6ab2ebda654
Benjamin Kaestner <benjamin.kaestner@gmail.com>
2015-09-24 20:30:19 +0200

    Add more tests

--- test/Text/SFCL/Render/SongSpec.hs
+++ test/Text/SFCL/Render/SongSpec.hs
@@ -1,17 +1,12 @@
 module Text.SFCL.Render.SongSpec (spec) where
 import           Text.SFCL.Render.Song (renderSong)
-import           Control.Applicative
-import           Data.Attoparsec.ByteString.Char8
-import           Data.ByteString.Char8 (ByteString)
-import qualified Data.ByteString.Char8 as B
 import           Text.SFCL
 import           Test.Hspec
-import           Test.Hspec.Attoparsec
 import           Test.QuickCheck
 
 spec :: Spec
-spec = describe "renderSong" $
-  it "should leave LBlocks" $ property $
+spec = describe "renderSong" $ do
+  it "should handle LBlocks" $ property $
     forAll (listOf1 nonCommand) $ \xs ->
       let
         lblock = LBlock xs
@@ -19,5 +14,33 @@ spec = describe "renderSong" $
         song   = Song [line]
       in renderSong song `shouldBe` xs
 
+  it "should handle multiple LBlocks" $ property $
+    forAll (listOf1 $ listOf1 alpha) $ \xs ->
+      let
+        lblocks = map LBlock xs
+        line    = Line lblocks
+        song    = Song [line]
+      in renderSong song `shouldBe` unwords xs
+
+  it "should handle CBlocks" $ property $
+    forAll (listOf1 nonCommand) $ \xs ->
+      let
+        cblock = CBlock xs
+        line   = Line [cblock]
+        song   = Song [line]
+      in renderSong song `shouldBe` "@chord{" ++ xs ++ "}"
+
+  it "should handle multiple CBlocks" $ property $
+    forAll (listOf1 $ listOf1 $ alpha) $ \xs ->
+      let
+        cblocks = map CBlock xs
+        line    = Line cblocks
+        song    = Song [line]
+        chorded = map (\x -> "@chord{" ++ x ++ "}") xs
+      in renderSong song `shouldBe` unwords chorded
+
 nonCommand :: Gen Char
 nonCommand = elements $ filter (`notElem` "@\r\n") [' '..'~']
+
+alpha :: Gen Char
+alpha = elements $ ['a'..'z'] ++ ['A'..'Z']
